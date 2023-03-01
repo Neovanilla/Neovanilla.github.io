b0288818569509f99de5bb44169f8233b06b4cc2
Franco Gonzalez <102259830+Neovanilla@users.noreply.github.com>
2023-02-28 21:06:02 -0300

    Initial Commit

---
+++ js/esconder_elemento.js
@@ -0,0 +1,11 @@
+function esconder_elemento() {
+    var x = document.getElementById("esconder");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+ 
+  
